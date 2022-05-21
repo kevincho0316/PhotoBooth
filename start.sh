fd649a7067d6629b587379de0c79b6de04b86fa0
kevincho <kevincho9029@gmail.com>
2022-05-21 15:08:55 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ mkdir -p depth/depth_result
 mkdir -p depth/depth_result_cut
 wget https://github.com/Sxela/ArcaneGAN/releases/download/v0.4/ArcaneGANv0.4.jit
 sudo apt install python3-pip
+pip3 install --upgrade pip
 pip3 install -r requirements.txt
 
