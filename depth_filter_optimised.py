6172a10e2bbdae140629e7c204ee62395bf087f8
kevincho <kevincho9029@gmail.com>
2022-05-30 00:29:11 +0900

    depth optimise

--- depth_filter_optimised.py
+++ depth_filter_optimised.py
@@ -57,23 +57,27 @@ def place(ori_img,mask_fd, id):
     background.save(out_dir)
     return out_dir
 
-def process(filedir, id):
-    model_type = "DPT_Large"     # MiDaS v3 - Large     (highest accuracy, slowest inference speed)
+
+model_type = "DPT_Large"     # MiDaS v3 - Large     (highest accuracy, slowest inference speed)
     # model_type = "DPT_Hybrid"   # MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
     #model_type = "MiDaS_small"  # MiDaS v2.1 - Small   (lowest accuracy, highest inference speed)
 
-    midas = torch.hub.load("intel-isl/MiDaS", model_type)
+midas = torch.hub.load("intel-isl/MiDaS", model_type)
 
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    midas.to(device)
-    midas.eval()
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+midas.to(device)
+midas.eval()
 
-    midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
 
-    if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
-        transform = midas_transforms.dpt_transform
-    else:
-        transform = midas_transforms.small_transform
+if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
+    transform = midas_transforms.dpt_transform
+else:
+    transform = midas_transforms.small_transform
+
+
+def process(filedir, id):
+    
 
     img = cv2.imread(filedir)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
